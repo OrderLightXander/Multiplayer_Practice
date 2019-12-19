fb0f0d5e592bcf4fcfd2288c464e41bac7765a03
OrderLightXander <OrderLight@users.noreply.github.com>
2019-12-19 15:14:28 +0100

    Initial commit

---
+++ Assets/Scripsts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    public float playerJumpHeight = 500f;
+    [SerializeField]
+    private float speed = 5f;
+    [SerializeField]
+    private float lookSensitivity = 3f;
+    private PlayerMotor motor;
+    public bool isGrounded;
+    private void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    private void Update()
+    {
+        float _xMov = Input.GetAxisRaw("Horizontal");
+        float _zMov = Input.GetAxisRaw("Vertical");
+        Vector3 _movJump = transform.up * playerJumpHeight;
+        Vector3 _movHorizontal = transform.right * _xMov;
+        Vector3 _movVertical = transform.forward * _zMov;
+        Vector3 _velocityWithJump = (_movHorizontal + _movVertical + _movJump).normalized * speed;
+        Vector3 _velocity = (_movHorizontal + _movVertical).normalized * speed;
+        if (isGrounded && Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().AddForce(Vector3.up * playerJumpHeight);
+            isGrounded = false;
+        }
+        motor.Move(_velocity);
+        float _yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        motor.Rotate(_rotation);
+
+
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _cameraRotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        motor.RotateCamera(_cameraRotation);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        isGrounded = true;
+    }
+}
